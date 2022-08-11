0b74eec1d61ed938e38f03839959a2ca2911e1a0
hanwool95 <77750865+hanwool95@users.noreply.github.com>
2022-08-12 05:29:18 +0900

    Fix: inheritance process

--- main.py
+++ main.py
@@ -2,17 +2,18 @@
 
 class Sentence_Box:
     def __init__(self, sentence: str = ""):
-        self._sentence = sentence
+        self._sentence = ""
         self._words = []
-        self.separate_sentence()
+        self.insert_sentence(sentence)
 
     def insert_sentence(self, sentence: str):
         self._sentence = sentence
+        self._separate_sentence()
 
     def get_sentence(self) -> str:
         return self._sentence
 
-    def separate_sentence(self):
+    def _separate_sentence(self):
         if self._sentence:
             self._words = self._sentence.split(" ")
         else:
@@ -24,7 +25,7 @@ class Sentence_Box:
 
 #1 Sentence Box 객체를 상속하는 단어 분석 객체 제작.
 #stop words list를 inint하면서 추가. stop words list = ["은", "는", "이", "가"]
-class Word_Analyzer(Sentence_Box):
+class Word_Cleaner(Sentence_Box):
     stop_words = ["은", "는", "이", "가"]
 
     def __init__(self, sentence: str = ""):
@@ -34,22 +35,31 @@ class Word_Analyzer(Sentence_Box):
     def get_words_count(self) -> int:
         return len(self._words)
 
-    #3 각 단어의 끝 문자가 stop_words에 있으면 stop word를 제거하는 함수 제작
-    def remove_stop_words(self):
-        for i, word in enumerate(self._words):
-            if word[-1] in self.stop_words:
-                self._words[i] = self._words[i][:-1]
+    #3 주어진 단어의 끝 문자가 stop_words에 있으면 stop word를 제거하여 반환하는 함수 제작
+    def remove_stop_words(self, word: str) -> str:
+        if word[-1] in self.stop_words:
+            word = word[:-1]
+        return word
 
+    #4 저장된 문장의 stopword를 제거하는 함수 제작. 3번에서 제작한 함수 응용
+    def reform_words(self):
+        for i, word in enumerate(self._words):
+            self._words[i] = self.remove_stop_words(word)
+
+    #5 insert_sentence 함수를 새롭게 제작. stop word를 제거하는 과정 추가. 4번에서 제작한 함수 응용.
+    def insert_sentence(self, sentence: str):
+        self._sentence = sentence
+        self._separate_sentence()
+        self.reform_words()
 
 def main_process():
     sentence = "안녕하세요 여러분 김한울입니다. 김한울은 학생입니다."
     box = Sentence_Box(sentence)
     print(box.get_words())
 
-    analyzer = Word_Analyzer(sentence)
-    print(analyzer.get_words_count())
-    analyzer.remove_stop_words()
-    print(analyzer.get_words())
+    cleaner = Word_Cleaner(sentence)
+    print(cleaner.get_words_count())
+    print(cleaner.get_words())
 
 if __name__ == '__main__':
     main_process()
